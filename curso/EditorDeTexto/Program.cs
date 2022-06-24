56564355250b3c4fb5c4c8b7624b7a779dd15073
Lucas Frenhan <95430926+LucasFrenhan@users.noreply.github.com>
2022-06-24 17:30:29 -0300

    Add files via upload

---
+++ curso/EditorDeTexto/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+
+namespace EditorDeTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 0:
+                    System.Environment.Exit(0);
+                    break;
+                case 1:
+                    Abrir();
+                    break;
+                case 2:
+                    Editar();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string caminho = Console.ReadLine();
+            Console.WriteLine("Qual o nome o arquivo?");
+            string nomeArquivo = Console.ReadLine();
+
+            using (var arquivo = new StreamReader(caminho + nomeArquivo + ".txt"))
+            {
+                string texto = arquivo.ReadToEnd();
+                Console.WriteLine(texto);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair) ");
+            Console.WriteLine("----------------------------------------");
+
+            string texto = "";
+
+            do
+            {
+                texto += Console.ReadLine();
+                texto += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(texto);
+        }
+
+        static void Salvar(string texto)
+        {
+            Console.Clear();
+            Console.WriteLine(" Qual caminho para salvar seu arquivo?");
+            var caminho = Console.ReadLine();
+            Console.WriteLine("Qual o nome do arquivo");
+            string nomeArquivo = Console.ReadLine();
+
+            using (var arquivo = new StreamWriter(caminho + nomeArquivo + ".txt"))
+            {
+                arquivo.Write(texto);
+            }
+
+            Console.WriteLine($" Arquivo {caminho} salvo com sucesso!!");
+            Menu();
+        }
+    }
+}
