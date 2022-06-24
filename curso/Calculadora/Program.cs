b1c7d1b9ead3a8f6a8a117765ced92583cab4500
Lucas Frenhan <95430926+LucasFrenhan@users.noreply.github.com>
2022-06-24 10:47:58 -0300

    Add files via upload

---
+++ curso/Calculadora/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Bem vindo a nossa calculdora");
+            Console.WriteLine("Escolha uma das opções abaixo:");
+            Console.WriteLine("1 - SOMA");
+            Console.WriteLine("2 - SUBTRAÇÃO");
+            Console.WriteLine("3 - MULTIPLICAÇÃO");
+            Console.WriteLine("4 - DIVISÃO");
+            Console.WriteLine("5 - SAIR");
+            short opcao;
+            opcao = short.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Soma();
+                    break;
+
+                case 2:
+                    Subtracao();
+                    break;
+
+                case 3:
+                    Multiplicacao();
+                    break;
+
+                case 4:
+                    Divisao();
+                    break;
+
+                case 5:
+                    Finalizador();
+                    break;
+
+                default:
+                    Console.WriteLine("Você digitou uma opção inválida");
+                    Menu();
+                    break;
+            }
+        }
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("SOMA");
+            Console.WriteLine("Digite o primeiro número");
+            float numero1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            float numero2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da soma dos dois números digitados é :{numero1 + numero2}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("SUBTRAÇÃO");
+            Console.WriteLine("Digite o primeiro número");
+            float numero1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            float numero2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da subtração do primeiro pelo segundo número é :{numero1 - numero2}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("MULTIPLICAÇÃO");
+            Console.WriteLine("Digite o primeiro número");
+            float numero1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            float numero2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da multiplicação dos dois números digitados é :{numero1 * numero2}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Divisao()
+
+        {
+            Console.Clear();
+            Console.WriteLine("DIVISÃO");
+            Console.WriteLine("Digite o primeiro número");
+            float numero1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            float numero2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da divisão do primeiro pelo segundo número é :{numero1 / numero2}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Finalizador()
+        {
+            Console.WriteLine("Obrigado por usar nosso sistema!");
+            Console.WriteLine("Saindo...");
+            System.Environment.Exit(0);
+        }
+    }
+}
---
+++ curso/Cronometro/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = segundo");
+            Console.WriteLine("M = minuto");
+            Console.WriteLine("0 = sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string dado = Console.ReadLine().ToLower();
+            char tipo = char.Parse(dado.Substring(dado.Length - 1, 1));
+            int tempo = int.Parse(dado.Substring(0, dado.Length - 1));
+            int multiplicador = 1;
+
+            if (tipo == 'm')
+                multiplicador = 60;
+
+            if (tempo == 0)
+                System.Environment.Exit(0);
+
+
+            PreInicio(tempo * multiplicador);
+
+        }
+
+        static void PreInicio(int tempo)
+        {
+            Console.Clear();
+            Console.WriteLine("Preparar...");
+            Thread.Sleep(800);
+            Console.Clear();
+            Console.WriteLine("Apontar...");
+            Thread.Sleep(800);
+            Console.Clear();
+            Console.WriteLine("Valendo!");
+            Thread.Sleep(800);
+
+            Inicio(tempo);
+        }
+        static void Inicio(int tempo)
+        {
+            int tempoAtual = 0;
+
+            while (tempoAtual != tempo)
+            {
+                Console.Clear();
+                tempoAtual++;
+                Console.WriteLine(tempoAtual + "s");
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Cronometro finalizado");
+            Thread.Sleep(1000);
+            Menu();
+        }
+    }
+}
